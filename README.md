f7b11f85801923d94b8e099ed62e909564d7a02b
ihni7 <ihnishat1998@gmail.com>
2020-10-20 10:00:14 +0600

    ADDED: README file

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
-# earthquake_visualizer
+# Earthquake Visualizer
 
-A new Flutter project.
+An earthquake visualizer made using Flutter and Dart ❤.
 
-## Getting Started
+## Website
 
-This project is a starting point for a Flutter application.
+To see the app in action, click [here](https://ikramhasan.github.io/Earthquake-Visualizer/#/)
 
-A few resources to get you started if this is your first Flutter project:
+## App preview
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+<br>
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<img src="screenshots/homepage.png" alt="drawing">
+
+## My Socials
+
+[![Twitter][1.1]][1]
+[![Facebook][2.1]][2]
+[![Github][3.1]][3]
+
+[1.1]: http://i.imgur.com/tXSoThF.png "twitter icon with padding"
+[2.1]: http://i.imgur.com/P3YfQoD.png "facebook icon with padding"
+[3.1]: http://i.imgur.com/0o48UoR.png "github icon with padding"
+
+## Thanks
+
+- [Coding Train](https://www.youtube.com/watch?v=HyAeZKWWuxA) for the [tutorial](https://www.youtube.com/watch?v=ZiYdOwOrGyc)
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'package:earthquake_visualizer/data/map_data.dart';
 import 'package:earthquake_visualizer/ui/visualizer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,9 +18,21 @@ class _HomePageState extends State<HomePage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Earthquake Visualizer",
-              style: TextStyle(fontSize: 22),
+            child: RichText(
+              text: TextSpan(
+                text: "Earthquake Visualizer",
+                style: GoogleFonts.ubuntu(fontSize: 22, color: Colors.white),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' by Ikram Hasan',
+                    style: GoogleFonts.ubuntu(
+                      fontSize: 22,
+                      color: Colors.grey,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Center(
@@ -38,58 +51,59 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  color: Colors.red,
-                  child: Text(
-                    'All Day',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      type = EarthquakeType.ALL_DAY;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  color: Colors.red,
-                  child: Text(
-                    'All Week',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      type = EarthquakeType.ALL_WEEK;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  color: Colors.red,
-                  child: Text(
-                    'All Month',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      type = EarthquakeType.ALL_MONTH;
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
+          buttonRow(),
         ],
       ),
     );
+  }
+
+  buttonRow() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: RaisedButton(
+          color: Colors.red,
+          child: Text(
+            'All Day',
+            style: GoogleFonts.ubuntu(fontSize: 16),
+          ),
+          onPressed: () {
+            setState(() {
+              type = EarthquakeType.ALL_DAY;
+            });
+          },
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: RaisedButton(
+          color: Colors.red,
+          child: Text(
+            'All Week',
+            style: GoogleFonts.ubuntu(fontSize: 16),
+          ),
+          onPressed: () {
+            setState(() {
+              type = EarthquakeType.ALL_WEEK;
+            });
+          },
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: RaisedButton(
+          color: Colors.red,
+          child: Text(
+            'All Month',
+            style: GoogleFonts.ubuntu(fontSize: 16),
+          ),
+          onPressed: () {
+            setState(() {
+              type = EarthquakeType.ALL_MONTH;
+            });
+          },
+        ),
+      ),
+    ]);
   }
 }
